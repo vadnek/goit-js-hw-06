041777121742df318d1153303b6585611995aa00
Vadim Lukyanenko <vadneek@yahoo.com>
2022-11-08 22:37:35 +0200

    tasks 1-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+// Обработка отправки формы form.login-form должна быть
+//  по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert 
+// с предупреждением о том, что все поля должны быть 
+// заполнены.
+// Если пользователь заполнил все поля и отправил форму,
+//  собери значения полей в обьект, где имя поля будет 
+//  именем свойства, а значение поля - значением свойства. 
+//  Для доступа к элементам формы используй свойство 
+//  elements.
+// Выведи обьект с введенными данными в консоль и 
+// очисти значения полей формы методом reset.
+
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+const {
+    elements : {email, password}
+} = event.currentTarget;
+    
+if (email.value === "" || password.value === "") {
+    return console.log("Bсе поля должны быть заполнены.");
+}
+
+    console.log(`email: ${email.value}, password: ${password.value}`);
+    event.currentTarget.reset();
+}
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// Напиши скрипт, который изменяет цвета фона элемента <body> 
+// через инлайн стиль при клике на button.change-color и 
+// выводит значение цвета в span.color.
+const btnOn = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+
+const onChangeColorBody = () => {
+ function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+const color = getRandomHexColor();
+
+bodyEl.style.backgroundColor = color;
+spanColor.textContent = color;
 }
+
+btnOn.addEventListener('click', onChangeColorBody);
+
+
+
+
+
